de128acee77a6943fc76a281fdff93c080a8fb9d
Mrugesh Wadagbalkar <mwadagbalkar@eisgroup.com>
2025-08-14 18:39:37 +0100

    added post review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -35,6 +35,15 @@ def get_request(endpoint, **kwargs):
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
+        
 # Add code for posting review
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
